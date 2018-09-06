e54f7c4d73caeece41bd45f744fe3ea3f47651d8
Victor Danilo <victordanilo_cs@live.com>
2018-09-06 19:23:17 -0300

    Add fix on scrollbar in opening bar-create-task

--- resources/src/js/project.js
+++ resources/src/js/project.js
@@ -4,11 +4,13 @@ $(function () {
             // open bar new task
             $(document).on('click', "#open-new-task", function () {
                 $("#create-task-wrapper").addClass('active');
+                $("#main-content").addClass('not-scrolling').trigger('reset-scrolling');
             });
-            
+
             // close bar new task
             $(document).on('click', '#btn-task-close', function () {
                 $("#create-task-wrapper").removeClass('active');
+                $("#main-content").removeClass('not-scrolling');
             });
         },
         stop: function () {
